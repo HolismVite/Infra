b37a1f394c6faff35618d8aec3d9d8971e114190
Saeed Nemati <sn.1361@gmail.com>
2022-06-07 17:11:17 +0430

    committed by script

--- src/Components/List/EntitiesCards.jsx
+++ src/Components/List/EntitiesCards.jsx
@@ -6,6 +6,7 @@ import Tooltip from '@mui/material/Tooltip';
 import Collapse from '@mui/material/Collapse';
 import { ListContext, app } from '@List';
 import NoEntitiesFound from '../NoEntitiesFound';
+import { EntityContext } from './List'
 
 const Cards = ({
     data,
@@ -75,67 +76,73 @@ const Cards = ({
                     >
                         {
                             data.map((item, index) =>
-                                <div
-                                    className=
-                                    {
-                                        'item w-full overflow-hidden group relative ' +
-                                        (multicolumn ? '' : "py-4 px-6 ") +
-                                        (!multicolumn && index !== 0 ? 'border-t ' : '') +
-                                        (classProvider ? classProvider(item) : '')
-                                    }
-                                    key={item.id}
-                                    dir={app.isRtl() ? "rtl" : "ltr"}
+                                <EntityContext.Provider
+                                    value={{
+                                        entity: item
+                                    }}
                                 >
-                                    {
-                                        hasItemSelection
-                                            ?
-                                            <div className="flex flex-row">
-                                                <div className="flex items-center justify-center w-10 mr-4">
-                                                    <Checkbox
-                                                        checked={selectedItems.indexOf(item.id) > -1}
-                                                        color="primary"
-                                                        onChange={(event) => {
-                                                            event.target.checked
-                                                                ?
-                                                                app.addItemToSelectedItems(listContext, item.id)
-                                                                :
-                                                                app.removeItemFromSelectedItems(listContext, item.id)
-                                                        }}
+                                    <div
+                                        className=
+                                        {
+                                            'item w-full overflow-hidden group relative ' +
+                                            (multicolumn ? '' : "py-4 px-6 ") +
+                                            (!multicolumn && index !== 0 ? 'border-t ' : '') +
+                                            (classProvider ? classProvider(item) : '')
+                                        }
+                                        key={item.id}
+                                        dir={app.isRtl() ? "rtl" : "ltr"}
+                                    >
+                                        {
+                                            hasItemSelection
+                                                ?
+                                                <div className="flex flex-row">
+                                                    <div className="flex items-center justify-center w-10 mr-4">
+                                                        <Checkbox
+                                                            checked={selectedItems.indexOf(item.id) > -1}
+                                                            color="primary"
+                                                            onChange={(event) => {
+                                                                event.target.checked
+                                                                    ?
+                                                                    app.addItemToSelectedItems(listContext, item.id)
+                                                                    :
+                                                                    app.removeItemFromSelectedItems(listContext, item.id)
+                                                            }}
+                                                        />
+                                                    </div>
+                                                    <div>
+                                                        {
+                                                            card(item)
+                                                        }
+                                                    </div>
+                                                </div>
+                                                :
+                                                card(item)
+                                        }
+                                        {
+                                            (entityActions || hasDelete || hasEdit || edit)
+                                                ?
+                                                <div
+                                                    className={(multicolumn ? " bg-white border absolute bottom-0 right-0 z-50 hidden group-hover:flex" : "")}
+                                                >
+                                                    <EntityActions
+                                                        entityType={entityType}
+                                                        item={item}
+                                                        entityActions={entityActions}
+                                                        menuForActions={menuForActions}
+                                                        hasDelete={hasDelete}
+                                                        hasEdit={hasEdit}
+                                                        edit={edit}
+                                                        create={create}
+                                                        upsert={upsert}
+                                                        setItem={setItem}
+                                                        reload={reload}
                                                     />
                                                 </div>
-                                                <div>
-                                                    {
-                                                        card(item)
-                                                    }
-                                                </div>
-                                            </div>
-                                            :
-                                            card(item)
-                                    }
-                                    {
-                                        (entityActions || hasDelete || hasEdit || edit)
-                                            ?
-                                            <div
-                                                className={(multicolumn ? " bg-white border absolute bottom-0 right-0 z-50 hidden group-hover:flex" : "")}
-                                            >
-                                                <EntityActions
-                                                    entityType={entityType}
-                                                    item={item}
-                                                    entityActions={entityActions}
-                                                    menuForActions={menuForActions}
-                                                    hasDelete={hasDelete}
-                                                    hasEdit={hasEdit}
-                                                    edit={edit}
-                                                    create={create}
-                                                    upsert={upsert}
-                                                    setItem={setItem}
-                                                    reload={reload}
-                                                />
-                                            </div>
-                                            :
-                                            null
-                                    }
-                                </div>
+                                                :
+                                                null
+                                        }
+                                    </div>
+                                </EntityContext.Provider>
                             )
                         }
                     </div>
