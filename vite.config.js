13b1585af46f3a28fd8ba2bff633cac420756348
Saeed Nemati <sn.1361@gmail.com>
2022-06-11 07:50:22 +0430

    committed by script

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
 const path = require(`path`)
 const aliases = {
   '@Form': 'src/Components/Form/Exports',
@@ -36,7 +38,7 @@ export default defineConfig(({ mode }) => {
       alias: resolvedAliases,
       preserveSymlinks: true
     },
-    plugins: [react(), htmlPlugin()],
+    plugins: [react(), htmlPlugin(), svgr()],
     server: {
       host: '0.0.0.0',
       hmr: {
