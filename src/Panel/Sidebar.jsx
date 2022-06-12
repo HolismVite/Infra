0797cea916dc362ee445f3d97c9d5aa7757ff8ba
Saeed Nemati <sn.1361@gmail.com>
2022-06-12 12:13:38 +0430

    committed by script

--- src/Panel/Sidebar.jsx
+++ src/Panel/Sidebar.jsx
@@ -11,26 +11,24 @@ import HolismIcon from '../Components/HolismIcon';
 export default function Sidebar({ onClick }) {
     const [open, setOpen] = useLocalStorageState(true, 'brandingAndMenuIsShown')
 
-    return (
-        <div className={"h-full dark:bg-slate-900 " + (open && " pt-4 ")}>
-            <div className={"cursor-pointer flex justify-center "}
-                onClick={() => setOpen(!open)}>
-                {
-                    open
-                        ?
-                        <HolismIcon icon={ExpandLessIcon} />
-                        :
-                        <HolismIcon icon={ExpandMoreIcon} />
-                }
-            </div>
-            <Collapse in={open}>
-                <Branding onClick={onClick} />
-                <User onClick={onClick} />
-            </Collapse>
-            <Menu
-                onClick={onClick}
-                className={open && " mt-5 "}
-            />
+    return <div className={"h-full overflow-y-auto bg-white dark:bg-slate-900 " + (open && " pt-4 ")}>
+        <div className={"cursor-pointer flex justify-center "}
+            onClick={() => setOpen(!open)}>
+            {
+                open
+                    ?
+                    <HolismIcon icon={ExpandLessIcon} />
+                    :
+                    <HolismIcon icon={ExpandMoreIcon} />
+            }
         </div>
-    );
+        <Collapse in={open}>
+            <Branding onClick={onClick} />
+            <User onClick={onClick} />
+        </Collapse>
+        <Menu
+            onClick={onClick}
+            className={open && " mt-5 "}
+        />
+    </div>
 }
