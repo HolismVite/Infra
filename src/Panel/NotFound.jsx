aa43c2bc4879ad5f4436430debb30d4ff532b5f0
Saeed Nemati <sn.1361@gmail.com>
2022-06-13 16:40:09 +0430

    committed by script

--- src/Panel/NotFound.jsx
+++ src/Panel/NotFound.jsx
@@ -15,8 +15,8 @@ const NotFound = () => {
 
     return <div className="flex flex-col items-center justify-center">
         <div className="text-9xl text-red-400 font-bold">404</div>
-        <div className="uppercase mt-10 text-6xl font-bold tracking-widest text-gray-600 text-center">NOT FOUND</div>
-        <div className="text-sm mt-10 text-gray-600 font-light text-center">The page you requested does not exist.<br />Please use the menu to navigate.<br />Or go to the home page.</div>
+        <div className={"uppercase mt-10 text-6xl font-bold text-gray-600 text-center " + (app.getLocale().supportsLetterSpacing && " tracking-widest ")}>{app.t('NOT FOUND')}</div>
+        <div className="text-sm mt-10 text-gray-600 font-light text-center">{app.t('The page you requested does not exist')}.<br />{app.t('Please use the menu to navigate')}.<br />{app.t('Or go to the home page')}.</div>
         <div className="mt-10">
             <Link
                 to={"/"}
@@ -25,7 +25,7 @@ const NotFound = () => {
                     className="bg-green-200 hover:bg-green-400 mt-2 lg:mt-0 mr-2"
                     variant="outlined"
                 >
-                    Home
+                    {app.t('Home')}
                 </Button>
             </Link>
         </div>
