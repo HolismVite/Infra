6a4055f71f2e3491c6356353ae0b14588bd84539
Saeed Nemati <sn.1361@gmail.com>
2022-06-15 19:03:28 +0430

    committed by script

--- src/Panel/Message.jsx
+++ src/Panel/Message.jsx
@@ -1,97 +1,97 @@
-import app from '../Base/App';
-import React, { useState, useEffect } from 'react';
-import Snackbar from '@mui/material/Snackbar';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-// import MuiAlert from '@material-ui/lab/Alert';
+import app from '../Base/App'
+import React, { useState, useEffect } from 'react'
+import Snackbar from '@mui/material/Snackbar'
+import IconButton from '@mui/material/IconButton'
+import CloseIcon from '@mui/icons-material/Close'
+import MuiAlert from '@mui/material/Alert'
 
-// function Alert(props) {
-//     return <MuiAlert elevation={6} variant="filled" {...props} />;
-// }
-// https://mui.com/components/snackbars/#customization
+const Alert = React.forwardRef((props, ref) => {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
 
 const Message = () => {
-    const [isShown, setIsShown] = useState();
-    const [message, setMessage] = useState();
-    const [action, setAction] = useState();
-    const [type, setType] = useState();
-    const [id, setId] = useState(app.randomId());
-    const [classes, setClasses] = useState('');
+    const [isShown, setIsShown] = useState()
+    const [message, setMessage] = useState()
+    const [action, setAction] = useState()
+    const [type, setType] = useState()
+    const [classes, setClasses] = useState('')
+    const [severity, setSeverity] = useState('')
 
     useEffect(() => {
         const show = ({ message, action, type }) => {
-            setMessage(message);
-            setAction(action);
-            setType(type);
+            setMessage(message)
+            setAction(action)
+            setType(type)
         }
-        app.on(app.showMessage, show);
+        app.on(app.showMessage, show)
         return () => {
-            app.removeListener(app.showMessage, show);
-        };
-    }, []);
+            app.removeListener(app.showMessage, show)
+        }
+    }, [])
 
     useEffect(() => {
         if (message) {
-            setId(app.randomId());
-            setIsShown(true);
+            setIsShown(true)
         }
-    }, [message]);
+    }, [message])
 
     useEffect(() => {
         if (type === 'success') {
-            setClasses('bg-green-400 text-white');
+            setClasses('bg-green-400 text-white')
+            setSeverity('success')
         }
         else if (type === 'info') {
-            setClasses('bg-blue-400 text-white');
+            setClasses('bg-blue-400 text-white')
+            setSeverity('info')
         }
         else if (type === 'warning') {
-            setClasses('bg-yellow-400 text-gray-900');
+            setClasses('bg-yellow-400 text-gray-900')
+            setSeverity('warning')
         }
         else if (type === 'error') {
-            setClasses('bg-red-400 text-white-900');
+            setClasses('bg-red-400 text-white-900')
+            setSeverity('danger')
         }
         else {
-            setClasses(null);
+            setClasses(null)
+            setSeverity('')
         }
-    }, [type]);
+    }, [type])
 
-    const hide = () => {
-        setMessage(null);
-        setIsShown(false);
+    const hide = (e, reason) => {
+        if (reason === 'clickaway') {
+            return
+        }
+        setIsShown(false)
     }
 
+    useEffect(() => {
+        if (isShown === false) {
+            setTimeout(() => {
+                setMessage(null)
+                setSeverity(null)
+            }, 100)
+        }
+    }, [isShown])
+
     return <Snackbar
+        open={isShown}
+        autoHideDuration={6000}
+        onClose={hide}
         anchorOrigin={{
             vertical: 'top',
             horizontal: 'center',
         }}
-        key={id}
-        type={type}
-        className={classes + ' rounded-md'}
         ContentProps={{
             className: classes,
             style: { whiteSpace: 'pre-line' }
         }}
-        open={isShown}
-        autoHideDuration={6000}
-        onClose={hide}
-        message={app.t(message)}
         bodystyle={{ whiteSpace: 'pre-line' }}
-        action={
-            <>
-                {
-                    action
-                        ?
-                        action
-                        :
-                        null
-                }
-                <IconButton size="small" aria-label="close" color="inherit" onClick={hide}>
-                    <CloseIcon fontSize="small" />
-                </IconButton>
-            </>
-        }
-    />
+    >
+        <Alert onClose={hide} severity={'success'} sx={{ width: '100%' }}>
+            {app.t(message)}
+        </Alert>
+    </Snackbar>
 }
 
-export default Message;
+export default Message
