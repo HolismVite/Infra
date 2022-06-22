894492143c8a304205c97d8ca6bf28fd61b87b8f
Saeed Nemati <sn.1361@gmail.com>
2022-06-22 09:04:38 +0430

    committed by script

--- src/Components/Show/DatePart.jsx
+++ src/Components/Show/DatePart.jsx
@@ -1,17 +1,20 @@
+// total mess: https://en.wikipedia.org/wiki/List_of_calendars
 import { format } from 'date-fns-jalali'
 import { app } from '@Panel'
 
 const DatePart = ({ value }) => {
     const normalizedValue = (value && value.endsWith('Z')) ? value : (value + 'Z');
-    return value ? <span>
-        {
-            app.getLocale().key === 'fa'
-                ?
-                format(new Date(normalizedValue), 'yyyy/MM/dd')
-                :
-                new Date(normalizedValue).toDateString()
-        }
-    </span> : null;
+    if (!value) {
+        return null
+    }
+    const localeKey = app.getLocale().key;
+    if (localeKey === 'fa') {
+       return format(new Date(normalizedValue), 'yyyy/MM/dd')
+    }
+    if (localeKey === 'ar') {
+        return new Intl.DateTimeFormat('ar-TN-u-ca-islamic', {day: 'numeric', month: 'long',weekday: 'long',year : 'numeric'}).format(Date.now());
+    }
+    new Date(normalizedValue).toDateString()
 }
 
 export { DatePart }
