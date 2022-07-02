68cfc76fb8f35ebc11b8c25ca32fe0096851ac4f
Saeed Nemati <sn.1361@gmail.com>
2022-07-02 13:59:41 +0430

    committed by script

--- src/Base/Api.jsx
+++ src/Base/Api.jsx
@@ -6,7 +6,7 @@ const axiosApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 })
 
-axiosApi.interceptors.request.use(config => {
+const requestInterceptor = config => {
   config.headers['Content-Type'] = 'application/json';
   config.headers['Accept'] = 'application/json';
   config.headers['X-Client'] = 'React';
@@ -17,69 +17,65 @@ axiosApi.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${app.token()}`;
     return Promise.resolve(config);
   })
+}
 
-  // config.headers.Authorization = `Bearer ${app.token()}`;
-  // return config;
-});
-
-axiosApi.interceptors.response.use(
-  response =>
-    response
-  ,
-  error => {
-    if (error.response === undefined) {
-      throw new Error(error.message ? error.toString() : 'Unknown error');
-    }
-    if (error.response.status === 401 || (error.response.status === 500 && error.response.data === "An error occured processing your authentication.")) {
-      var url = new URL(app.createLogoutUrl());
-      url.search = app.createLoginUrl();
-      window.newUrl = url;
-      app.checkLogin();
-      throw new Error('You need to login again.');
-    }
-    if (error.response.status === 403) {
-      throw new Error('you are logged in, but you do not have access to this section');
-      // todo: redirect user to "403" page.
-    }
-    if (error.response.status === 404) {
-      throw new Error('404\nService does not exist');
-    }
-    if (error.response.status === 400 || error.response.status === 500) {
-      var messages = '';
-      var data = error.response.data;
-      if (typeof data !== "string") {
-        for (var item in error.response.data) {
-          if (item.toLocaseCase && item.toLocaseCase() === 'type') {
-            continue;
-          }
-          if (Array.isArray(data[item])) {
-            for (var i = 0; i < data[item].length; i++) {
-              messages += data[item][i] + "\n";
-            }
-          }
-          else if (typeof data[item] === 'object') {
-            console.log(data[item]);
-          }
-          else {
-            messages += data[item] + "\n";
+const errorInterceptor = error => {
+  if (error.response === undefined) {
+    throw new Error(error.message ? error.toString() : 'Unknown error');
+  }
+  if (error.response.status === 401 || (error.response.status === 500 && error.response.data === "An error occured processing your authentication.")) {
+    var url = new URL(app.createLogoutUrl());
+    url.search = app.createLoginUrl();
+    window.newUrl = url;
+    app.checkLogin();
+    throw new Error('You need to login again.');
+  }
+  if (error.response.status === 403) {
+    throw new Error('you are logged in, but you do not have access to this section');
+    // todo: redirect user to "403" page.
+  }
+  if (error.response.status === 404) {
+    throw new Error('404\nService does not exist');
+  }
+  if (error.response.status === 400 || error.response.status === 500) {
+    var messages = '';
+    var data = error.response.data;
+    if (typeof data !== "string") {
+      for (var item in error.response.data) {
+        if (item.toLocaseCase && item.toLocaseCase() === 'type') {
+          continue;
+        }
+        if (Array.isArray(data[item])) {
+          for (var i = 0; i < data[item].length; i++) {
+            messages += data[item][i] + "\n";
           }
         }
+        else if (typeof data[item] === 'object') {
+          console.log(data[item]);
+        }
+        else {
+          messages += data[item] + "\n";
+        }
       }
-      else {
-        messages = data;
-      }
-      if (messages.indexOf('IDX10223') > -1) {
-        app.checkLogin();
-        throw new Error('You need to login again.');
-        //app.updateToken();
-      }
-      console.log(messages);
-      throw new Error(messages);
     }
+    else {
+      messages = data;
+    }
+    if (messages.indexOf('IDX10223') > -1) {
+      app.checkLogin();
+      throw new Error('You need to login again.');
+      //app.updateToken();
+    }
+    console.log(messages);
+    throw new Error(messages);
   }
-)
+}
 
-export async function get(url) {
+axiosApi.interceptors.request.use(requestInterceptor);
+
+axiosApi.interceptors.response.use(response => response, errorInterceptor)
+
+const get = async (url) => {
   return await
     axiosApi.get(url, {
       crossDomain: true
@@ -88,13 +84,13 @@ export async function get(url) {
     })
 }
 
-export async function post(url, data) {
+const post = async (url, data) => {
   return await axiosApi
     .post(url, Array.isArray(data) ? [...data] : { ...data })
     .then(response => response?.data)
 }
 
-export async function upload(url, data) {
+const upload = async (url, data) => {
   return await axiosApi
     .post(url, data, {
       headers: {
@@ -103,3 +99,10 @@ export async function upload(url, data) {
     })
     .then(response => response?.data)
 }
+
+export { axios }
+export { errorInterceptor }
+export { get }
+export { post }
+export { requestInterceptor }
+export { upload }
--- src/Panel/HeaderActions/ClearCache.jsx
+++ src/Panel/HeaderActions/ClearCache.jsx
@@ -1,8 +1,26 @@
-import { post } from '../../Base/Api'
+import {
+    axios,
+    errorInterceptor,
+    requestInterceptor,
+} from '../../Base/Api'
 import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
 import HeaderAction from "./HeaderAction"
 
 const ClearCache = () => {
+
+    const axiosApi = axios.create({
+        baseURL: import.meta.env.VITE_SITE_API_URL
+    })
+
+    axiosApi.interceptors.request.use(requestInterceptor);
+
+    axiosApi.interceptors.response.use(response => response, errorInterceptor)
+
+    const post = async (url, data) => {
+        return await axiosApi
+            .post(url)
+            .then(response => response?.data)
+    }
 
     return <HeaderAction
         title="Clear Cache"
