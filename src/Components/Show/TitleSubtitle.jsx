b2bb2ad68d9a2232dc76f710ad20b975690e6239
Saeed Nemati <sn.1361@gmail.com>
2022-07-07 13:13:59 +0430

    committed by script

--- src/Components/Show/TitleSubtitle.jsx
+++ src/Components/Show/TitleSubtitle.jsx
@@ -1,7 +1,13 @@
-import app from 'App'
-
-const TitleSubtitle = ({ title, subtitle }) => {
+const TitleSubtitle = ({
+    subtitle,
+    supertitle,
+    title,
+}) => {
     return <div className={"ltr:text-left rtl:text-right"}>
+        {
+            supertitle &&
+            <div className="text-xs text-gray-400">{supertitle}</div>
+        }
         <div className="text-lg font-bold text-slate-600 dark:text-slate-400">{title}</div>
         <div className="text-xs text-gray-400">{subtitle}</div>
     </div>
