db3ea6892d80b8ebff1cf6a8d133bfa6760c3f7b
Saeed Nemati <sn.1361@gmail.com>
2022-07-11 12:39:12 +0430

    committed by script

--- src/Components/List/Properties/NumberProperty.jsx
+++ src/Components/List/Properties/NumberProperty.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import EditIcon from '@mui/icons-material/Edit';
+import { post } from 'App'
+import { ListContext } from 'Contexts'
+import { DialogContext } from 'Contexts'
 import DialogForm from '../../Form/DialogForm'
-import DialogContext from 'Contexts'
+import Numeric from '../../Form/Fields/Numeric'
+import HolismIcon from '../../HolismIcon'
 
 const NumberProperty = ({
     actionUrl,
     value,
 }) => {
 
-    const [open, setIsOpen] = useState(false)
+    const [open, setOpen] = useState(false)
+
+    const { setEntity } = useContext(ListContext)
+
+    const save = ({
+        data,
+        error,
+        setProgress,
+        success,
+    }) => {
+        if (!actionUrl || app.isNothing(actionUrl)) {
+            return;
+        }
+        setProgress(true);
+        var api = actionUrl;
+        if (typeof actionUrl === 'function') {
+            api = actionUrl(data.value);
+        }
+        post(api).then(data => {
+            setProgress(false);
+            success('Applied');
+            setEntity(data)
+            setOpen(false)
+        }, e => {
+            error(e);
+            setProgress(false);
+        });
+    }
 
     return <DialogContext.Provider
         value={{
@@ -16,25 +48,24 @@ const NumberProperty = ({
         }}
     >
         <span
-            className={actionUrl && "cursor-pointer"}
+            className={actionUrl && "cursor-pointer flex gap-2 justify-center items-center group"}
             title={actionUrl && 'Click to change'}
             onClick={() => actionUrl && setOpen(true)}
         >
             <span>{value || 'NA'}</span>
+            {
+                actionUrl && <HolismIcon
+                    icon={EditIcon}
+                    className="text-sm group-hover:text-red-400"
+                />
+            }
         </span>
         <DialogForm
             title='Edit'
             inputs={<Numeric
-                column='Value'
+                column='value'
             />}
-            okAction={({
-                data,
-                error,
-                setProgress,
-                success,
-            }) => {
-                console.log(data)
-            }}
+            okAction={save}
         />
 
     </DialogContext.Provider>
