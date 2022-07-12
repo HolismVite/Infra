97d2b61fc8c2789ad9a79c4f94cc6372c646f122
Saeed Nemati <sn.1361@gmail.com>
2022-07-12 19:59:16 +0430

    committed by script

--- src/Components/Browse/BrowserDialog.jsx
+++ src/Components/Browse/BrowserDialog.jsx
@@ -5,17 +5,53 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import Collapse from '@mui/material/Collapse';
 import CloseIcon from '@mui/icons-material/Close';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import CheckIcon from '@mui/icons-material/Check';
+import app from 'App'
+import { useLocalStorageState } from 'Hooks'
+import { useList } from 'Hooks'
 import { DialogContext } from 'Contexts'
+import { ListContext } from 'Contexts'
 import { BrowseContext } from 'Contexts'
 import Unify from '../Unify';
 import Pagination from '../List/Pagination';
+import Filtering from "../List/Filtering";
+import Sorting from "../List/Sorting";
+import Entities from "../List/Entities";
+import EntityAction from '../List/EntityActions/EntityAction';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-const BrowserDialog = () => {
+const BrowserDialog = ({
+    card,
+    entityType,
+    filters,
+    headers,
+    isTree,
+    row,
+    sorts,
+}) => {
+
+    const [isFilteringOpen, setIsFilteringOpen] = useLocalStorageState(false, `${app.camelize(entityType)}_isFilteringOpen`);
+
+    const {
+        data,
+        hasData,
+        loading,
+        metadata,
+        reload,
+    } = useList({
+        entityType,
+        isTree
+    })
+
+    const toggleFiltering = () => {
+        setIsFilteringOpen(!isFilteringOpen);
+    }
 
     const {
         open,
@@ -23,39 +59,101 @@ const BrowserDialog = () => {
     } = useContext(DialogContext)
 
     const {
-        list
+        close,
+        onSelected,
+        selectedEntity,
+        setSelectedEntity,
     } = useContext(BrowseContext)
 
-    return <Dialog
-        open={open}
-        fullScreen
-        TransitionComponent={Transition}
-        onClose={() => setOpen(false)}
-    >
-        <DialogTitle
-            className="bg-gray-100"
+    const entityActions = <>
+        <EntityAction
+            icon={<CheckIcon />}
+            title='Select'
+            click={({ entity }) => {
+                setSelectedEntity(entity)
+                if (onSelected instanceof Function) {
+                    onSelected(entity)
+                }
+                if (close instanceof Function) {
+                    close()
+                }
+            }}
+        />
+    </>
+
+    const list = <>
+        <div className='flex items-center justify-end px-6 py-2'>
+            <div className="flex items-center">
+                {
+                    sorts
+                        ?
+                        <Sorting sorts={sorts} />
+                        :
+                        null
+                }
+                {
+                    filters && (filters.props?.children?.length > 0 || filters.props?.children?.props)
+                        ?
+                        <span
+                            className={listActionIconStyle + " mr-4"}
+                            onClick={toggleFiltering}><FilterListIcon /></span>
+                        :
+                        null
+                }
+            </div>
+        </div>
+
+        <Collapse in={isFilteringOpen}>
+            <div className='mb-4'>
+                <Filtering filters={filters} />
+            </div>
+        </Collapse>
+
+        <Entities />
+    </>
+
+    return <ListContext.Provider value={{
+        card,
+        data,
+        entityActions,
+        hasData,
+        headers,
+        isBrowse: true,
+        loading,
+        metadata,
+        reload,
+        row,
+    }}>
+        <Dialog
+            open={open}
+            fullScreen
+            TransitionComponent={Transition}
+            onClose={() => setOpen(false)}
         >
-            <div className="flex items-center justify-between">
-                <div
-                    className="flex gap-4 items-center"
-                >
-                    <IconButton
-                        onClick={() => setOpen(false)}
+            <DialogTitle
+                className="bg-gray-100"
+            >
+                <div className="flex items-center justify-between">
+                    <div
+                        className="flex gap-4 items-center"
                     >
-                        <CloseIcon />
-                    </IconButton>
-                    <span className="ml-4">{app.t("Find")}</span>
-                </div>
-                <div
-                    dir='ltr'
-                    className="listActions flex-1 flex gap-4"
-                >
-                    {/* <IconButton
+                        <IconButton
+                            onClick={() => setOpen(false)}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                        <span className="ml-4">{app.t("Find")}</span>
+                    </div>
+                    <div
+                        dir='ltr'
+                        className="listActions flex-1 flex gap-4"
+                    >
+                        {/* <IconButton
                     onClick={() => setOpen(false)}
                 >
                     <CloseIcon />
                 </IconButton> */}
-                    {/* <IconButton
+                        {/* <IconButton
                     onClick={() => setOpen(false)}
                 >
                     <CloseIcon />
@@ -65,16 +163,17 @@ const BrowserDialog = () => {
                 >
                     <CachedIcon />
                 </IconButton> */}
+                    </div>
                 </div>
-            </div>
-        </DialogTitle>
-        <DialogContent>
-            <Unify component={list} />
-        </DialogContent>
-        <DialogActions>
-            <Pagination />
-        </DialogActions>
-    </Dialog>
+            </DialogTitle>
+            <DialogContent>
+                <Unify component={list} />
+            </DialogContent>
+            <DialogActions>
+                <Pagination />
+            </DialogActions>
+        </Dialog>
+    </ListContext.Provider>
 }
 
 export default BrowserDialog
--- src/Components/Browse/Exports.jsx
+++ src/Components/Browse/Exports.jsx
@@ -1,11 +1,9 @@
 import Ascending from '../List/Sorts/Ascending'
 import Enum from '../List/Filters/Enum'
 import Image from '../List/Properties/Image'
-import List from './List'
 import Text from '../List/Filters/Text'
 
 export { Ascending }
 export { Enum }
 export { Image }
-export { List }
 export { Text }
